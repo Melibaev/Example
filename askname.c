ff5fc61deddf40c6d771a4e41f8754c87dd840f8
Melibaev <145578471+Melibaev@users.noreply.github.com>
2023-09-29 14:39:45 +0500

    Update askname.c
    Доработка

--- askname.c
+++ askname.c
@@ -5,6 +5,7 @@
 void askname(char *first, char *last)
 {
     printf("Enter your first name: ");
+    printf("hello world");
     fgets(first, 255, stdin);
     first[strlen(first)-1] = '\0'; /* remove the newline at the end */
 
